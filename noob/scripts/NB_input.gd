bbd8a1325e39d5672c2f7aa84f16ffb6391cfb08
officialErnests <n0o0b090lv@gmail.com>
2026-02-21 11:16:47 +0200

    double mic wip calibration system

--- noob/scripts/NB_input.gd
+++ noob/scripts/NB_input.gd
@@ -5,17 +5,23 @@ extends Node
 var effect : AudioEffect
 var effect_2 : AudioEffect
 var recording
-var timer = 0
-var max_vol = 1
+var timer:float = 0
+var max_vol: float= 1
+var max_vol_2: float= 1
+var input_device = 0
 @export var visualiser: Sprite2D
+@export var visualiser_2: Sprite2D
+var audio_input_devices
+var input_one_device = 2
+var input_two_device = 3 
 
 func _ready() -> void:
 	var idx = AudioServer.get_bus_index("mic_1")
-	var idx_2 = AudioServer.get_bus_index("mic_2")
 	effect = AudioServer.get_bus_effect(idx, 0)
 	effect.format = 0
-
-	effect_2 = AudioServer.get_bus_effect(idx_2, 0)
+	audio_input_devices = AudioServer.get_input_device_list()
+	print(audio_input_devices)
+	AudioServer.set_input_device(audio_input_devices[0])
 	effect.set_recording_active(true)
 	# await get_tree().create_timer(1).timeout
 	# for i in effect.get_recording().data:
@@ -32,7 +38,7 @@ func _process(delta: float) -> void:
 		var temp_recording = effect.get_recording()
 		if temp_recording:
 			# if !effect.is_empty():?\
-			var avg = 0
+			var avg: float = 0
 			var index = -1
 			for i in temp_recording.data:
 				index += 1
@@ -41,9 +47,13 @@ func _process(delta: float) -> void:
 				avg += abs(i)
 				# if i > 0:
 				# 	print(i)
-			if avg > max_vol:
-				max_vol = avg
-			visualiser.global_position.x = float(avg) / float(max_vol) * float(1000)
-			print(avg," ", max_vol, " ", avg / max_vol)
+			if avg >  (max_vol if input_device == 0 else max_vol_2):
+				if input_device == 0: 	max_vol = avg
+				else: 					max_vol_2 = avg
+			if input_device == 0: 	visualiser.global_position.x = avg / max_vol * 1000
+			else: 					visualiser_2.global_position.x = avg / max_vol_2 * 1000
+			print(avg, " ", input_device, " ", (max_vol if input_device == 0 else max_vol_2))
 			effect.set_recording_active(false)
+			input_device = (input_device + 1) % 2
+			AudioServer.set_input_device(audio_input_devices[input_one_device if input_device == 0 else input_two_device])
 			effect.set_recording_active(true)
