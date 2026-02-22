6d5b42e2e057d27651a478d06c06b7ab6194acae
officialErnests <n0o0b090lv@gmail.com>
2026-02-22 19:47:22 +0200

    IT WORKSSSS

--- noob/scenes/continue_button.gd
+++ noob/scenes/continue_button.gd
@@ -4,4 +4,5 @@ func _on_pressed() -> void:
 	var temp = preload("res://scenes/main.tscn")
 	var temp2 = temp.instantiate()
 	$"../../../".add_child(temp2)
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	$"../../".queue_free()
+	temp2.get_node("greg_logic").nb_input = $"../../../Input"
